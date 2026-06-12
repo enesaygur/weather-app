28144b0c3f0bf3668d2e7cd304f97b832b56acfb
Enes <enesaygur387@gmail.com>
2026-06-13 00:32:54 +0300

    format forecast dates

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -3,12 +3,19 @@ interface Props {
   weather: WeatherData;
 }
 function ForecastCard({ weather }: Props) {
+  const formatDate = (date: string) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "short",
+      day: "numeric",
+    });
+  };
   return (
     <div>
       <h2>Day Forecast</h2>
       {weather.daily.time.map((day, index) => (
         <div key={day}>
-          <p>{day}</p>
+          <p>{formatDate(day)}</p>
           <p>
             {weather.daily.temperature_2m_min[index]}°C /
             {weather.daily.temperature_2m_max[index]}°C
